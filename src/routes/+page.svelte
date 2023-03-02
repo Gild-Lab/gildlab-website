7b0ec801e31acd1cb55938e19312d08b0be93be1
Nino Keldishvili <Ninokeldishvili@gmail.com>
2023-03-02 16:29:54 +0400

    Table styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,7 +37,8 @@
           they hold e.g. income and the decentralised finance community.
         </p>
 
-        <p><span class="fw-700">Email </span> David at <span class="fw-700"> gildlab.xyz</span> if you would like to become a customer of Gild
+        <p><span class="fw-700">Email </span> David at <span class="fw-700"> gildlab.xyz</span> if you would like to
+          become a customer of Gild
           Lab.
         </p>
       </div>
@@ -49,39 +50,57 @@
         <p>
           Personal judgements directed based upon accountability, not persuasion. Both layers are accessible. This is
           unique and highly sought after by traders.
-
         </p>
-
-<!--        <div class="chart-table">-->
-<!--          <table>-->
-<!--            <thead>-->
-<!--            <tr>-->
-<!--              <th></th>-->
-<!--              <th></th>-->
-<!--              <th>Regular View</th>-->
-<!--              <th>SFT View</th>-->
-<!--            </tr>-->
-<!--            </thead>-->
-<!--            <tbody>-->
-<!--            <tr>-->
-<!--              <td>Words</td>-->
-<!--              <td>Trustworthy</td>-->
-<!--              <td>DAO <br/> + <br/> Marketplace </td>-->
-<!--              <td></td>-->
-<!--            </tr>-->
-<!--            <tr>-->
-<!--              <td>Actions</td>-->
-<!--              <td>Rain Contracts <br/>Produces</td>-->
-<!--              <td>Trust Vault</td>-->
-<!--              <td>SFT <br/>-->
-<!--                (guild labs contracts)</td>-->
-<!--              <td>All data-->
-<!--                resides here</td>-->
-<!--            </tr>-->
-<!--            </tbody>-->
-<!--          </table>-->
-<!--        </div>-->
       </div>
+      <div class="chart-table">
+        <div class="eye-icon">
+          <svg width="108" height="78" viewBox="0 0 108 78" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+                d="M74.25 39C74.25 49.7696 65.1838 58.5 54 58.5C42.8162 58.5 33.75 49.7696 33.75 39C33.75 28.2304 42.8162 19.5 54 19.5C56.0754 19.5 58.0779 19.8007 59.963 20.3592C56.447 21.8878 54 25.2937 54 29.25C54 34.6348 58.5331 39 64.125 39C68.2334 39 71.7703 36.6437 73.3578 33.2578C73.9378 35.0732 74.25 37.0015 74.25 39Z"
+                fill="black"/>
+            <path fill-rule="evenodd" clip-rule="evenodd"
+                  d="M54 78C13.5 78 0 39 0 39C0 39 13.5 0 54 0C94.5 0 108 39 108 39C108 39 94.5 78 54 78ZM14.817 39.28L14.6726 39L14.817 38.72C16.3758 35.7178 18.7902 31.7436 22.1835 27.8224C28.8869 20.0762 38.8948 13 54 13C69.1052 13 79.1131 20.0762 85.8165 27.8224C89.2098 31.7436 91.6242 35.7178 93.183 38.72L93.3274 39L93.183 39.28C91.6242 42.2822 89.2098 46.2564 85.8165 50.1776C79.1131 57.9238 69.1052 65 54 65C38.8948 65 28.8869 57.9238 22.1835 50.1776C18.7902 46.2564 16.3758 42.2822 14.817 39.28Z"
+                  fill="black"/>
+          </svg>
+        </div>
+        <table class="fw-700">
+          <thead>
+          <tr>
+            <th></th>
+            <th></th>
+            <th colspan="2">Regular View</th>
+            <th>SFT View</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td class="br-3">Words</td>
+            <td class="br-3 bt-3 bb-3 bg-green">Trustworthy</td>
+            <td class="bt-3 br-3 bb-3 bg-green">DAO <br/> + <br/> Marketplace</td>
+          </tr>
+          <tr>
+            <td class="br-3">Actions</td>
+            <td class="br-3 bb-3 bg-brown">Transparency <br/><span class="fw-400">Accountability</span></td>
+            <td colspan="3" class="br-3 bt-3 bb-3 bg-brown">
+              <div class="d-flex justify-between  trust">
+                <div> Rain Contracts <br/><span class="fw-400">Produces</span></div>
+                <div>Trust Vault</div>
+                <div>SFT<br/>
+                  <span class="fw-400">(guild labs contracts)</span> <br/>
+                </div>
+                <div> All data
+                  resides here
+                </div>
+              </div>
+
+
+            </td>
+
+          </tr>
+          </tbody>
+        </table>
+      </div>
+
       <div class="text">
         <p>If a public transparent layer was added to companies, accountability would trend.
         </p>
@@ -100,7 +119,8 @@
           linked in baskets.
         </p>
         <p>
-          <span class="fw-700">Email </span> David at <span class="fw-700"> gildlab.xyz</span> if you would like to use the Semi-Fungible
+          <span class="fw-700">Email </span> David at <span class="fw-700"> gildlab.xyz</span> if you would like to use
+          the Semi-Fungible
           System.
         </p>
       </div>
@@ -177,26 +197,82 @@
         margin-left: -100px;
         margin-right: -100px;
         background-color: #EBEBD3;
-        padding: 1rem 4rem;
+        padding: 1rem 8rem;
         font-size: 18px;
         margin-top: 5rem;
     }
 
     .chart p {
-        width: 40%;
+        width: 60%;
         line-height: 26px;
     }
 
-    table, td, th {
-        border: 3px solid #000000;
+    .chart-table {
+        margin-left: -100px;
+        margin-right: -100px;
     }
 
     table {
+        position: relative;
+        font-size: 18px;
+        font-family: 'Noto Sans JP', sans-serif;
         width: 100%;
         border-collapse: collapse;
         text-align: center;
     }
 
+    .eye-icon {
+        position: absolute;
+        right: 38%;
+        margin-top: -7rem;
+    }
+
+    tr {
+        height: 128px;
+    }
+
+    .br-3 {
+        border-right: 3px solid #000000
+    }
+
+    .bt-3 {
+        border-top: 3px solid #000000
+    }
+
+    .bl-3 {
+        border-left: 3px solid #000000
+    }
+
+    .bb-3 {
+        border-bottom: 3px solid #000000
+    }
+
+    .d-flex {
+        display: flex;
+    }
+
+    .justify-between {
+        justify-content: space-between;
+    }
+
+    .trust div {
+        width: 20%;
+    }
+
+    .trust {
+        text-align: left;
+        padding-left: 1rem;
+        padding-right: 1rem;
+        align-items: center;
+    }
+
+    .bg-green {
+        background: #A7CACC;
+    }
+
+    .bg-brown {
+        background: #D9D093;
+    }
 
     @media only screen and (max-width: 600px) {
 
