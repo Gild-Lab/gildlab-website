a08a3f1be0267ee1208ef47b09ed69df4db26911
Nino Keldishvili <Ninokeldishvili@gmail.com>
2023-03-06 16:51:28 +0400

    sveltejs-adapter-ipfs

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,12 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from 'sveltejs-adapter-ipfs';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true,
+			fallback: '200.html'
 		})
-	},
-	preprocess: vitePreprocess()
+	}
 };
 
 export default config;
